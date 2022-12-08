e29f7fde80f8636af4be0b246776e78063694ca3
DanilVelichko <velichko.danil@gmail.com>
2022-12-08 18:34:32 +0100

    simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const createPhotoRender = (gallery) => {
+    
+    return gallery.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>
+      `;
+    }).join('');
+};
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPhotoRender(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup); 
+
+let lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+
 console.log(galleryItems);
+
