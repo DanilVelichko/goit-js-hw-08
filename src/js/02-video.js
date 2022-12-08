2b120f4a1db6ba8950fc279c96c85d56092077a2
DanilVelichko <velichko.danil@gmail.com>
2022-12-08 21:07:40 +0100

    task 2 save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
- const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
 
     player.on('play', function() {
@@ -8,3 +8,18 @@
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
+
+    player.on('timeupdate', function(data) {
+    // data is an object containing properties specific to that event
+});
+
+// Avoid excessively updating the position while scrolling.
+jQuery(window).on('scroll', _.throttle(updatePosition, 1000));
+ 
+// Invoke `renewToken` when the click event is fired, 
+// but not more than once every 5 minutes.
+let throttled = _.throttle(renewToken, 300000, { 'trailing': false });
+jQuery(element).on('click', throttled);
+ 
+// Cancel the trailing throttled invocation.
+jQuery(window).on('popstate', throttled.cancel);
