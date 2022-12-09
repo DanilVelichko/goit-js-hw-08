30b9d492e633f0c2d0af98026ed209dbcda5f5af
DanilVelichko <velichko.danil@gmail.com>
2022-12-09 21:02:54 +0100

    Task 2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,22 @@
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
+const player = new Vimeo.Player(iframe);
+    
+player.on('timeupdate', function(getData) { 
+    localStorage.setItem("videoplayer-current-time", getData.seconds);//get currenttime  
     });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
-    player.on('timeupdate', function(data) {
-    // data is an object containing properties specific to that event
-});
 
-// Avoid excessively updating the position while scrolling.
-jQuery(window).on('scroll', _.throttle(updatePosition, 1000));
- 
-// Invoke `renewToken` when the click event is fired, 
-// but not more than once every 5 minutes.
-let throttled = _.throttle(renewToken, 300000, { 'trailing': false });
-jQuery(element).on('click', throttled);
- 
-// Cancel the trailing throttled invocation.
-jQuery(window).on('popstate', throttled.cancel);
+// Second variant 
+
+    
+// player.on('timeupdate', function(getData) { 
+//     localStorage.setItem("videoplayer-current-time", getData.seconds);//get currenttime
+// });
+//  player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+    
+// player.on('timeupdate', function(getData) { 
+//     localStorage.setItem("videoplayer-current-time", getData.seconds);//get currenttime
+// });
+//  player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
