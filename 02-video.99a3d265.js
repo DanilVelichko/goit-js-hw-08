c1e51edae21556698e6aa610959cd1d02afff5be
DanilVelichko <105587925+DanilVelichko@users.noreply.github.com>
2022-12-11 12:27:31 +0000

    Deploying to gh-pages from @ DanilVelichko/goit-js-hw-08@4925771b54fb66230d889ce4963ae2e8864e71b9 🚀

--- 02-video.99a3d265.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",e.seconds)})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"))}();
-//# sourceMappingURL=02-video.99a3d265.js.map
