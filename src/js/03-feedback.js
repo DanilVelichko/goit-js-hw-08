126e61cf9ee832b376a2ef06ab7a5a6e3279a308
DanilVelichko <velichko.danil@gmail.com>
2022-12-13 22:19:02 +0100

    finally done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,40 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const localParse = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const inputObject = {email: '', message: '',};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('input', throttle(onEmailInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+formOutput();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    console.log(inputObject);
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onEmailInput(e) {
+    inputObject.email = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputObject));
+  }
+
+function onTextareaInput(e) {
+      inputObject.message = e.target.value;
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(inputObject));
+}
+
+function formOutput() {
+    if (localParse) {
+        refs.email.value = localParse.email;
+        refs.textarea.value = localParse.message;
+    }
+}
